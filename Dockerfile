5f3673c8ab9e136c402c0ae7b3ada2e4ae4d6232
Hanwen Wu <steinwaywhw@gmail.com>
2016-09-08 13:06:48 -0400

    latest

--- Dockerfile
+++ Dockerfile
@@ -3,88 +3,23 @@ MAINTAINER Steinway Wu "https://github.com/steinwaywhw/docker-ats"
 
 # for installing erlang/elixir
 WORKDIR /tmp
-RUN apt-get update
-RUN apt-get install -y wget
+RUN apt-get -qq update
+RUN apt-get -qq install -y wget libgmp3-dev libgc-dev make gcc build-essential git && \
+				bash libjson-c-dev pkg-config mit-scheme openjdk-8-jdk unzip
 RUN wget http://packages.erlang-solutions.com/erlang-solutions_1.0_all.deb
 RUN dpkg -i erlang-solutions_1.0_all.deb
+RUN apt-get -qq update
+RUN apt-get -qq install -yq esl-erlang elixir
+RUN apt-get clean
 
-RUN apt-get update
-RUN apt-get install -y libgmp3-dev libgc-dev make gcc build-essential git bash libjson-c-dev esl-erlang elixir pkg-config
+RUN wget http://repo1.maven.org/maven2/org/clojure/clojure/1.8.0/clojure-1.8.0.zip
+RUN unzip clojure-1.8.0.zip && rm clojure-1.8.0.zip
+RUN echo "export CLASSPATH=\${HOME}/clojure-1.8.0/*:\${CLASSPATH}:" >> ${HOME}/.bashrc
 
-# ats2 and contrib source code
-WORKDIR /
-RUN git clone git://git.code.sf.net/p/ats2-lang/code ats2
-RUN git clone https://github.com/githwxi/ATS-Postiats-contrib.git ats2-contrib
-RUN git clone -b smtlib https://github.com/wdblair/ATS-Postiats-contrib ats2-contrib-smtlib
+ADD ./install.sh /tmp/install.sh 
+RUN chmod +x install.sh && ./install.sh
 
-# env
-ENV PATSHOME /ats2
-ENV PATH ${PATH}:/ats2/bin
-
-# build ats2
-WORKDIR /ats2
-RUN ./configure
-RUN make all
-
-# install z3
-WORKDIR /
-RUN git clone https://github.com/Z3Prover/z3
-WORKDIR /z3
-RUN apt-get install -y python
-RUN CXX=clang++ CC=clang python scripts/mk_make.py
-WORKDIR /z3/build
-RUN make
-RUN make install
-
-# build smt solve
-ENV PATSHOMERELOC /ats2-contrib-smtlib
-ENV PATSHOME_contrib /ats2-contrib-smtlib
-
-WORKDIR ${PATSHOMERELOC}/projects/MEDIUM/ATS-extsolve
-RUN make DATS_C
-WORKDIR ${PATSHOMERELOC}/projects/MEDIUM/ATS-extsolve-smt
-RUN make build
-RUN mv -f patsolve_smt ${PATSHOME}/bin
-
-# build z3 solve
-ENV PATSHOMERELOC /ats2-contrib
-ENV PATSHOME_contrib /ats2-contrib
-
-WORKDIR ${PATSHOMERELOC}/projects/MEDIUM/ATS-extsolve
-RUN make DATS_C
-WORKDIR ${PATSHOMERELOC}/projects/MEDIUM/ATS-extsolve-z3
-RUN make build
-RUN mv -f patsolve_z3 ${PATSHOME}/bin
-
-# build parse-emit
-WORKDIR ${PATSHOMERELOC}/projects/MEDIUM/CATS-parsemit
-RUN make DATS_C
-
-# build 2js
-WORKDIR ${PATSHOMERELOC}/projects/MEDIUM/CATS-atsccomp/CATS-atscc2js
-RUN make build
-RUN mv -f atscc2js ${PATSHOME}/bin
-WORKDIR ${PATSHOMERELOC}/contrib/libatscc/libatscc2js
-RUN make all 
-RUN make all_in_one
-
-# build 2erl
-WORKDIR ${PATSHOMERELOC}/projects/MEDIUM/CATS-atsccomp/CATS-atscc2erl 
-RUN make build
-RUN mv -f atscc2erl ${PATSHOME}/bin
-WORKDIR ${PATSHOMERELOC}/contrib/libatscc/libatscc2erl
-RUN make all 
-RUN make all_in_one
-WORKDIR ${PATSHOMERELOC}/contrib/libatscc/libatscc2erl/Session 
-RUN make all 
-RUN make all_in_one
-
-# install em
-# RUN apt-get install python-pip
-# RUN pip install em
-
-#RUN echo "export PATSHOME=/ats2" > env.sh
-#RUN echo "export PATSHOMERELOC=/ats2-contrib" >> env.sh
-#RUN echo "export PATH=$PATH:/ats2/bin" >> env.sh
-WORKDIR ${HOME}
+WORKDIR /root
 ENTRYPOINT /bin/bash
+
+
---
+++ install.sh
@@ -0,0 +1,170 @@
+#!/usr/bin/env sh
+
+######
+#
+# A shell script for
+# installing ATS2 + ATS2-contrib
+#
+######
+#
+# Author: Hongwei Xi
+# Authoremail: gmhwxiATgmailDOTcom
+#
+######
+
+######
+#
+# for installing ats-anairiats
+#
+######
+
+export GCC=gcc
+
+######
+#
+export ATSVER=0.2.12
+export ATSPACK=ats-lang-anairiats-${ATSVER}
+export ATSPACKTGZ=${ATSPACK}.tgz
+#
+######
+#
+export \
+ATSLANGURL_srcfg=\
+http://sourceforge.net/projects/ats-lang
+export \
+ATSLANGURL_github=http://ats-lang.github.io
+#
+######
+#
+wget -q \
+${ATSLANGURL_github}/ATS-Anairiats/${ATSPACKTGZ}
+#
+tar -zxf ${ATSPACKTGZ}
+#
+export ATSHOME=${PWD}/${ATSPACK}
+export ATSHOMERELOC=ATS-${ATSVER}
+#
+(cd ${ATSHOME} && ./configure && make CC=${GCC} all_ngc)
+#
+(cd $ATSHOME/bootstrap1 && rm -f *.o)
+(cd $ATSHOME/ccomp/runtime/GCATS && make && make clean)
+#
+######
+
+git clone https://github.com/githwxi/ATS-Postiats ATS2
+git clone https://github.com/githwxi/ATS-Postiats-contrib.git ATS2-contrib
+
+######
+
+export PATSHOME=${PWD}/ATS2
+export PATSHOMERELOC=${PWD}/ATS2-contrib
+
+######
+#
+export PATH=${PATSHOME}/bin:${PATH}
+#
+######
+#
+echo "export ATSHOME=${PWD}/${ATSPACK}" >> ${HOME}/.bashrc
+echo "export ATSHOMERELOC=ATS-${ATSVER}" >> ${HOME}/.bashrc 
+#
+echo "export PATSHOME=${PWD}/ATS2" >> ${HOME}/.bashrc
+echo "export PATSHOMERELOC=${PWD}/ATS2-contrib" >> ${HOME}/.bashrc
+echo "export PATSHOME_contrib=${PWD}/ATS2-contrib" >> ${HOME}/.bashrc
+#
+echo "export PATH=\${PATSHOME}/bin:\${PATH}" >> ${HOME}/.bashrc
+#
+######
+#
+# Building patsopt + patscc
+#
+(cd ATS2 && cp ${ATSHOME}/config.h .)
+(cd ATS2 && make -f Makefile_devl)
+#
+######
+#
+# Installing patscc and patsopt
+#
+# (cd ATS2 && make install)
+#
+######
+#
+# For libatsopt
+#
+(cd ATS2/src && make cleanall)
+#
+(cd ATS2/src/CBOOT && make -C prelude)
+(cd ATS2/src/CBOOT && make -C libc)
+(cd ATS2/src/CBOOT && make -C libats)
+(cd ATS2/utils/libatsopt && make && make clean)
+#
+cp ATS2/utils/libatsopt/libatsopt.a ${ATSHOME}/ccomp/lib
+#
+######
+#
+# For libatsynmark
+#
+(cd ATS2/utils/libatsynmark && make && make clean)
+#
+cp ATS2/utils/libatsynmark/libatsynmark.a ${ATSHOME}/ccomp/lib
+#
+######
+
+apt-get install -y erlang
+apt-get install -y libjson-c-dev
+
+######
+#
+# For parsing constraints 
+#
+(cd ATS2-contrib/projects/MEDIUM/ATS-extsolve && make DATS_C)
+#
+# For building patsolve_z3
+(cd ATS2-contrib/projects/MEDIUM/ATS-extsolve/ATS-extsolve-z3 && make build)
+(cd ATS2-contrib/projects/MEDIUM/ATS-extsolve/ATS-extsolve-z3 && mv -f patsolve_z3 ${PATSHOME}/bin)
+#
+# For building patsolve_smt2
+(cd ATS2-contrib/projects/MEDIUM/ATS-extsolve/ATS-extsolve-smt2 && make build)
+(cd ATS2-contrib/projects/MEDIUM/ATS-extsolve/ATS-extsolve-smt2 && mv -f patsolve_smt2 ${PATSHOME}/bin)
+#
+######
+#
+# For parsing C code
+# generated from ATS source
+#
+(cd ATS2-contrib/projects/MEDIUM/CATS-parsemit && make DATS_C)
+#
+# For building atscc2js
+#
+(cd ATS2-contrib/projects/MEDIUM/CATS-atsccomp/CATS-atscc2js && make build)
+(cd ATS2-contrib/projects/MEDIUM/CATS-atsccomp/CATS-atscc2js && mv -f atscc2js ${PATSHOME}/bin)
+(cd ATS2-contrib/contrib/libatscc/libatscc2js && make all && make all_in_one)
+#
+# For building atscc2py3
+#
+(cd ATS2-contrib/projects/MEDIUM/CATS-atsccomp/CATS-atscc2py3 && make build)
+(cd ATS2-contrib/projects/MEDIUM/CATS-atsccomp/CATS-atscc2py3 && mv -f atscc2py3 ${PATSHOME}/bin)
+(cd ATS2-contrib/contrib/libatscc/libatscc2py3 && make all && make all_in_one)
+#
+# For building atscc2scm
+#
+(cd ATS2-contrib/projects/MEDIUM/CATS-atsccomp/CATS-atscc2scm && make build)
+(cd ATS2-contrib/projects/MEDIUM/CATS-atsccomp/CATS-atscc2scm && mv -f atscc2scm ${PATSHOME}/bin)
+(cd ATS2-contrib/contrib/libatscc/libatscc2scm && make all && make all_in_one)
+#
+# For building atscc2clj
+#
+(cd ATS2-contrib/projects/MEDIUM/CATS-atsccomp/CATS-atscc2clj && make build)
+(cd ATS2-contrib/projects/MEDIUM/CATS-atsccomp/CATS-atscc2clj && mv -f atscc2clj ${PATSHOME}/bin)
+(cd ATS2-contrib/contrib/libatscc/libatscc2clj && make all && make all_in_one)
+#
+# For building atscc2erl
+#
+(cd ATS2-contrib/projects/MEDIUM/CATS-atsccomp/CATS-atscc2erl && make build)
+(cd ATS2-contrib/projects/MEDIUM/CATS-atsccomp/CATS-atscc2erl && mv -f atscc2erl ${PATSHOME}/bin)
+(cd ATS2-contrib/contrib/libatscc/libatscc2erl && make all && make all_in_one)
+(cd ATS2-contrib/contrib/libatscc/libatscc2erl/Session && make all && make all_in_one)
+#
+######
+#
+###### end of [C9-ATS2-install-cs520-latest.sh] ######
